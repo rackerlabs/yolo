1ce7d68b98f8d82401641ef14d6076c3263a9bc0
Lars Butler <Lars.Butler@gmail.com>
2018-04-24 11:48:33 +0200

    commands WIP

--- yolo/commands.py
+++ yolo/commands.py
@@ -2,6 +2,7 @@ import os
 
 import yolo.const
 import yolo.context
+import yolo.exceptions
 import yolo.yolo_file
 
 
@@ -26,6 +27,43 @@ def _get_yolo_file(yolo_file_path):
 
     # self._yolo_file_path = config_path
     return yolo.yolo_file.YoloFile.from_path(path)
+
+
+def get_creds_provider(yolo_file, account=None, stage=None):
+    if account is not None:
+        account_cfg = yolo_file.accounts.get(account)
+    elif stage is not None:
+        # Is there an explicit or implict account here?
+
+        ###########
+        # Implicit:
+        ###########
+        import yolo.credentials.aws
+        stage_cfg = yolo_file.stages.get(
+            # Get specific stage, if one is defined in the yolo config:
+            stage,
+            # Or, use the default one:
+            yolo_file.stages.get('default')
+        )
+        account_cfg = yolo_file.accounts.get(stage_cfg.account)
+    else:
+        raise yolo.exceptions.YoloError('No account or stage info given')
+
+    if account_cfg.credentials.provider == 'aws':
+        return yolo.credentials.aws.AWSCredentialsProvider(
+            profile_name=account_cfg.credentials.profile
+        )
+    elif account_cfg.credentials.provider == 'faws':
+        raise Exception('not implemented')
+        # return yolo.credentials.faws.FAWSCredentialsProvider(
+        #     ???
+        #)
+    else:
+        raise yolo.exception.YoloError(
+            'Unknown credentials provider "{}"'.format(
+                account_cfg.credentials.provider
+            )
+        )
 
 
 def deploy_infra(yolo_file_path, stage=None, account=None, dry_run=False,
@@ -60,8 +98,10 @@ def deploy_infra(yolo_file_path, stage=None, account=None, dry_run=False,
         If ``true``, tear down and re-create the stack from scratch.
         Otherwise, just try to update the existing stack.
     """
+    # 0. Load the initial yolo config:
     yolo_file = _get_yolo_file(yolo_file_path)
 
+    # 1. Command-specific validation/logic
     with_stage = stage is not None
     with_account = account is not None
 
@@ -78,41 +118,17 @@ def deploy_infra(yolo_file_path, stage=None, account=None, dry_run=False,
                 'safety purposes). You will need to tear down the stack '
                 'manually.'
             )
-        if 'account' not in self.yolo_file.templates:
+        if 'account' not in yolo_file.templates:
             raise YoloError('No "account" templates are defined.')
 
-    # Figure out the credentials provider to use:
-    if account is not None:
-        pass
-        # TODO:
-    elif stage is not None:
-        pass
-        # Is there an explicit or implict account here?
 
-        ###########
-        # Implicit:
-        ###########
-        import yolo.credentials.aws
-        stage_cfg = yolo_file.stages.get(
-            # Get specific stage, if one is defined in the yolo config:
-            stage,
-            # Or, use the default one:
-            yolo_file.stages.get('default')
-        )
-        account_cfg = yolo_file.accounts.get(stage_cfg.account)
+    # 2. Figure out the credentials provider to use:
+    creds_provider = get_creds_provider(
+        yolo_file, account=account, stage=stage
+    )
 
-        if account_cfg.credentials.provider == 'aws':
-            creds_provider = yolo.credentials.aws.AWSCredentialsProvider(
-                profile_name=account_cfg.credentials.profile
-            )
-
-        import pdb; pdb.set_trace()
-        ###########
-        # Explicit:
-        ###########
-
-    # Set up runtime context for rendering context variables in the yolo file
-    # (i.e., filling in the blanks):
+    # 3. Set up runtime context for rendering context variables in the yolo
+    #    file (i.e., filling in the blanks):
     # TODO: need the creds provider for this/might have to hit some AWS/FAWS
     # APIs
     # TODO: need to hit CloudFormation API to get `stage_outputs` and
@@ -126,3 +142,14 @@ def deploy_infra(yolo_file_path, stage=None, account=None, dry_run=False,
         stage_region=None,
         stage_outputs=None,
     )
+
+    # 4. Render the complete yolo file:
+    yolo_file = yolo_file.render(**context)
+
+    # 5. Run the command
+    if stage is not None:
+        # Deploy stage level templates:
+        pass
+    else:
+        # Deploy account-level templates:
+        pass
