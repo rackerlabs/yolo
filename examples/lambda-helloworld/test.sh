70e353f03412160bbe393b8c3c3ef99b806c5dc2
Lars Butler <Lars.Butler@gmail.com>
2018-07-11 23:56:24 +0200

    examples: add lambda smoketest script

---
+++ examples/lambda-helloworld/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+STG=dev
+SVC=MyLambdaService
+
+yolo deploy-infra --stage ${STG}
+yolo build-lambda --stage ${STG} --service ${SVC}
+yolo push --stage ${STG} --service ${SVC}
+
+yolo put-parameters --stage ${STG} --service ${SVC} --use-defaults
+yolo show-parameters --stage ${STG} --service ${SVC}
+yolo ensure-parameters --stage ${STG} --service ${SVC}
+
+VERSION=$(git rev-parse HEAD)
+yolo deploy-lambda --stage ${STG} --service ${SVC} --version ${VERSION}
+
+yolo show-service --stage ${STG} --service ${SVC}
